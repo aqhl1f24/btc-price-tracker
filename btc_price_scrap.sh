f67e34ac3f8dee02daaeb9d1bcc57f23067bda66
Aaron Lim <qihenglim66@gmail.com>
2025-11-28 21:55:38 +0800

    second commit

--- btc_price_scrap.sh
+++ btc_price_scrap.sh
@@ -11,6 +11,7 @@ echo "$timestamp, Getting price..."
 
 curl -s $URL -o "temp.html" 
 
+#get price
 price=$(grep -oE 'data-test="text-cdp-price-display">\$[0-9,]+(\.[0-9]+)?' "temp.html" | sed -E 's/.*data-test="text-cdp-price-display">//; s/[$,]//g')
 echo "Price current: $price"
 
@@ -19,3 +20,29 @@ echo "Price 24h low: $pricelow"
 
 pricehigh=$(grep -oE 'eQBACe label">High</div><span>\$[0-9,]+(\.[0-9]+)?' temp.html | sed -E 's#.QBACe label">High</div><span>##; s/[$,]//g')
 echo "Price 24h high: $pricehigh"
+
+check if bitcoin price can be extracted to avoid null
+if [[ -z "$price" || -z "$pricelow" || -z "$pricehigh" ]]; then
+	echo "$timestamp Error: Failed to get Bitcoin price."
+	rm temp.html
+	exit 1
+fi
+
+#Create database
+$MYSQL -e "CREATE DATABASE IF NOT EXISTS $DB;"
+
+$MYSQL $DB -e"
+CREATE TABLE IF NOT EXISTS BTC_PRICES(
+	price_id int auto_increment primary key,
+	timestamp datetime not null,
+	price decimal(10, 2),
+	low_24h decimal(10, 2),
+	high_24h decimal(10, 2)
+);"
+
+#insert values into database
+insert_query="
+INSERT INTO BTC_PRICES(timestamp, prie, low_24h, high_24h)
+VALUES ("$timestamp", "$price", "$pricelow", $pricehigh");"
+
+$MYSQL $DB -e "$insert_query"
